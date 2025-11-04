0949d4ac3d415a11f1dbdd25e5fa45ffffdc26c5
Arya Duta G S P <98201015+aryadgsp@users.noreply.github.com>
2025-11-04 10:01:54 +0700

    Update app layout
    1. Create dropdown menu bar
    2. Create footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,21 +54,9 @@
                                             </a>
                                         </li>
                                         <li class="header-item">
-                                            <a href="{{ url('/projects') }}" class="header-link hstack gap-2 fs-7 fw-bold text-dark">
+                                            <a href="{{ url('/portfolio') }}" class="header-link hstack gap-2 fs-7 fw-bold text-dark">
                                                 <img src="{{ asset('assets/images/svgs/secondary-leaf.svg') }}" alt="" width="20" height="20"
-                                                    class="img-fluid animate-spin">Projects
-                                            </a>
-                                        </li>
-                                        <li class="header-item">
-                                            <a href="{{ url('/blog') }}" class="header-link hstack gap-2 fs-7 fw-bold text-dark">
-                                                <img src="{{ asset('assets/images/svgs/secondary-leaf.svg') }}" alt="" width="20" height="20"
-                                                    class="img-fluid animate-spin">Blog
-                                            </a>
-                                        </li>
-                                        <li class="header-item">
-                                            <a href="{{ url('/services') }}" class="header-link hstack gap-2 fs-7 fw-bold text-dark">
-                                                <img src="{{ asset('assets/images/svgs/secondary-leaf.svg') }}" alt="" width="20" height="20"
-                                                    class="img-fluid animate-spin">Services
+                                                    class="img-fluid animate-spin">Portfolio
                                             </a>
                                         </li>
                                         <li class="header-item">
@@ -77,23 +65,11 @@
                                                     class="img-fluid animate-spin">Contact
                                             </a>
                                         </li>
-                                        <li class="header-item">
-                                            <a href="{{ url('/docs') }}" class="header-link hstack gap-2 fs-7 fw-bold text-dark">
-                                                <img src="{{ asset('assets/images/svgs/secondary-leaf.svg') }}" alt="" width="20" height="20"
-                                                    class="img-fluid animate-spin">Docs
-                                            </a>
-                                        </li>
                                     </ul>
-                                    <div class="hstack gap-3">
-                                        <a href="{{ url('/sign-in') }}"
-                                            class="btn btn-outline-light fs-6 bg-white px-3 py-2 text-dark w-50 hstack justify-content-center">Sign In</a>
-                                        <a href="{{ url('/sign-up') }}"
-                                            class="btn btn-dark text-white fs-6 bg-dark px-3 py-2 w-50 hstack justify-content-center">Sign Up</a>
-                                    </div>
                                 </div>
                                 <div>
-                                    <a class="text-dark" href="tel:+1-212-456-7890">+1-212-456-7890</a>
-                                    <a class="fs-8 text-dark fw-bold" href="mailto:info@wrappixel.com">info@wrappixel.com</a>
+                                    <a class="text-dark" href="tel:+6289691009400">+6289691009400</a>
+                                    <a class="fs-8 text-dark fw-bold" href="mailto:info@gadgetnio.com">info@gadgetnio.com</a>
                                 </div>
                             </div>
                         </ul>
@@ -112,27 +88,24 @@
                     <div class="d-flex flex-column gap-8 pe-xl-5">
                         <h2 class="mb-0 text-white">Build something together?</h2>
                         <div class="d-flex flex-column gap-2">
-                            <a href="https://www.wrappixel.com/" target="_blank" class="link-hover hstack gap-3 text-white fs-5">
+                            <a href="https://www.gadgetnio.com/" target="_blank" class="link-hover hstack gap-3 text-white fs-5">
                                 <iconify-icon icon="lucide:arrow-up-right" class="fs-7 text-primary"></iconify-icon>
-                                info@wrappixel.com
+                                Gadgetnio
                             </a>
-                            <a href="https://maps.app.goo.gl/hpDp81fqzGt5y4bC8" target="_blank"
+                            <a href="https://maps.app.goo.gl/vN3kjhX9WRJ3AgSU9" target="_blank"
                                 class="link-hover hstack gap-3 text-white fs-5">
                                 <iconify-icon icon="lucide:map-pin" class="fs-7 text-primary"></iconify-icon>
-                                info@wrappixel.com
+                                Gadgetnio
                             </a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4 col-xl-2 mb-8 mb-xl-0">
                     <ul class="footer-menu list-unstyled mb-0 d-flex flex-column gap-2">
-                        <li><a class="link-hover fs-5 text-white" href="index.html">Home</a></li>
-                        <li><a class="link-hover fs-5 text-white" href="about-us.html">About</a></li>
-                        <li><a class="link-hover fs-5 text-white" id="services" href="#services">Services</a></li>
-                        <li><a class="link-hover fs-5 text-white" href="projects.html">Work</a></li>
-                        <li><a class="link-hover fs-5 text-white" href="terms-and-conditions.html">Terms</a></li>
-                        <li><a class="link-hover fs-5 text-white" href="privacy-policy.html">Privacy Policy</a></li>
-                        <li><a class="link-hover fs-5 text-white" href="404.html">Error 404</a></li>
+                        <li><a class="link-hover fs-5 text-white" href="/">Home</a></li>
+                        <li><a class="link-hover fs-5 text-white" href="/about-us">About</a></li>
+                        <li><a class="link-hover fs-5 text-white" href="/portfolio">Portfolio</a></li>
+                        <li><a class="link-hover fs-5 text-white" href="/contact">Contact</a></li>
                     </ul>
                 </div>
                 <div class="col-md-4 col-xl-2 mb-8 mb-xl-0">
@@ -143,11 +116,11 @@
                     </ul>
                 </div>
                 <div class="col-md-4 col-xl-3 mb-8 mb-xl-0">
-                    <p class="mb-0 text-white text-opacity-70 text-md-end">© Studiova copyright 2025</p>
+                    <p class="mb-0 text-white text-opacity-70 text-md-end">© Gadgetnio copyright 2025</p>
                 </div>
             </div>
         </div>
-        <p class="mb-0 text-white text-opacity-70 text-md-center mt-10">Distributed by <a class="text-white" href="https://www.themewagon.com" target="_blank">ThemeWagon</a></p>
+        <p class="mb-0 text-white text-opacity-70 text-md-center mt-10">Created by <a class="text-white" href="https://www.agdgetnio.com" target="_blank">Gadgetnio Team</a></p>
     </footer>
 
     <div class="get-template hstack gap-2">
