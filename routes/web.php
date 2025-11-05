f4bff0a7919a6b1e2e5888ae8308ed6e5e9bff91
Arya Duta G S P <98201015+aryadgsp@users.noreply.github.com>
2025-11-05 13:14:39 +0700

    Create Routes & File

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/about-us', function () {
+    return view('about-us');
+});
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
