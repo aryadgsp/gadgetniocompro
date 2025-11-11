f91605ecb48d585e51d30a1cfb632163c4314cc5
Arya Duta G S P <arya.duta.gor-2021@ftmm.unair.ac.id>
2025-11-11 14:39:47 +0700

    Update about-us.blade.php
    update text color

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -32,18 +32,18 @@
             <div class="d-flex flex-column gap-5 gap-xl-11 gap-xxl-12">
                 <div class="row gap-4 gap-lg-0">
                     <div class="col-lg-4">
-                        <h2 class="fs-13 mb-0" data-aos="fade-right" data-aos-delay="200" data-aos-duration="1000">Gadgetnio.</h2>
+                        <h2 class="fs-13 mb-0" data-aos="fade-right" data-aos-delay="200" data-aos-duration="1000">Gadgetnio Group.</h2>
                     </div>
                     <div class="col-lg-8">
                         <div class="d-flex flex-column gap-4 gap-lg-5" data-aos="fade-up" data-aos-delay="200"
                             data-aos-duration="1000">
-                            <p class="mb-0 fs-5 text-dark">
+                            <p class="mb-0 fs-5 text-black">
                                 Gadgetnio Group is a leading electronics and gadget distributor in Indonesia,
                                 connecting world-class brands with local markets. We specialize in delivering
                                 high-quality products, efficient logistics, and long-term partnerships that empower
                                 retailers, resellers, and corporate clients to grow in today’s digital era.
                             </p>
-                            <p class="mb-0 fs-5 text-dark">
+                            <p class="mb-0 fs-5 text-black">
                                 With years of experience and a strong nationwide network, we’ve built our reputation on trust,
                                 authenticity, and innovation. Whether it’s the latest smartphones or essential electronic devices,
                                 Gadgetnio Group ensures every product and partnership meets the highest standards of reliability.
@@ -91,99 +91,99 @@
         <div class="marquee w-100 d-flex align-items-center overflow-hidden bg-primary py-4">
             <div class="marquee-content d-flex align-items-center gap-8">
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Integrity</h4>
+                    <h4 class="mb-0 text-white">Integrity</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Innovation</h4>
+                    <h4 class="mb-0 text-white">Innovation</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Partnership</h4>
+                    <h4 class="mb-0 text-white">Partnership</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Customer Focus</h4>
+                    <h4 class="mb-0 text-white">Customer Focus</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Integrity</h4>
+                    <h4 class="mb-0 text-white">Integrity</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Innovation</h4>
+                    <h4 class="mb-0 text-white">Innovation</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Partnership</h4>
+                    <h4 class="mb-0 text-white">Partnership</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Customer Focus</h4>
+                    <h4 class="mb-0 text-white">Customer Focus</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Integrity</h4>
+                    <h4 class="mb-0 text-white">Integrity</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Innovation</h4>
+                    <h4 class="mb-0 text-white">Innovation</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Partnership</h4>
+                    <h4 class="mb-0 text-white">Partnership</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Customer Focus</h4>
+                    <h4 class="mb-0 text-white">Customer Focus</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Integrity</h4>
+                    <h4 class="mb-0 text-white">Integrity</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Innovation</h4>
+                    <h4 class="mb-0 text-white">Innovation</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Partnership</h4>
+                    <h4 class="mb-0 text-white">Partnership</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Customer Focus</h4>
+                    <h4 class="mb-0 text-white">Customer Focus</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Integrity</h4>
+                    <h4 class="mb-0 text-white">Integrity</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Innovation</h4>
+                    <h4 class="mb-0 text-white">Innovation</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Partnership</h4>
+                    <h4 class="mb-0 text-white">Partnership</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Customer Focus</h4>
+                    <h4 class="mb-0 text-white">Customer Focus</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Integrity</h4>
+                    <h4 class="mb-0 text-white">Integrity</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Innovation</h4>
+                    <h4 class="mb-0 text-white">Innovation</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Partnership</h4>
+                    <h4 class="mb-0 text-white">Partnership</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
                 <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Customer Focus</h4>
+                    <h4 class="mb-0 text-white">Customer Focus</h4>
                     <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
                 </div>
             </div>
