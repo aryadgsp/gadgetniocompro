b33f03c1691b74cce396be5a0276e10de1128313
Arya Duta G S P <arya.duta.gor-2021@ftmm.unair.ac.id>
2025-11-12 10:08:51 +0700

    Update web.php
    add route group for partner detail page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ Route::get('/portfolio', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Route::prefix('partner')->group(function () {
+    Route::get('/ugreen', function () {
+        return view('partner.ugreen');
+    })->name('partner.ugreen');
+
+    Route::get('/vention', function () {
+        return view('partner.vention');
+    })->name('partner.vention');
+
+    Route::get('/redmi', function () {
+        return view('partner.redmi');
+    })->name('partner.redmi');
+});
