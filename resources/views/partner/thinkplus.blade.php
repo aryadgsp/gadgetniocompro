57c8a10653cb9967d05192f34cda53803334e913
Arya Duta G S P <arya.duta.gor-2021@ftmm.unair.ac.id>
2025-11-18 14:17:28 +0700

    Update thinkplus.blade.php

--- resources/views/partner/thinkplus.blade.php
+++ resources/views/partner/thinkplus.blade.php
@@ -22,7 +22,7 @@
                     </div>
                 </div>
                 <div class="d-flex align-items-end gap-3" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
-                    <h1 class="mb-0 fs-16 text-white lh-1">THINKPLUS</h1>
+                    <h1 class="mb-0 fs-15 fs-md-16 text-white lh-1">THINKPLUS</h1>
                     <a href="https://www.thinkplusaudio.id/" class="p-1 ps-7 bg-primary rounded-pill">
                         <span class="bg-white round-52 rounded-circle d-flex align-items-center justify-content-center">
                             <iconify-icon icon="lucide:arrow-up-right" class="fs-8 text-dark"></iconify-icon>
