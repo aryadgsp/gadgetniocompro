e5f6ea759fd9513f6c36baaf65b4c3505a189d27
Arya Duta G S P <98201015+aryadgsp@users.noreply.github.com>
2025-11-22 13:27:04 +0700

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,7 +18,7 @@ class ContactController extends Controller
         ]);
 
         // kirim email
-        Mail::to("emailtest@webmynddev.com")->send(new ContactMail($request->all()));
+        Mail::to("info@gadgetnio.com")->send(new ContactMail($request->all()));
 
         return back()->with('success', 'Your message has been sent!');
     }
